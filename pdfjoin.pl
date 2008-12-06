cc7830650c5c76f28c2259e39d201eebce017d23
Michael Stapelberg <michael+x200@stapelberg.de>
2008-12-06 18:35:13 +0100

    Bücher mit mehr als 10 Kapiteln und ohne Front/Back werden unterstützt

--- pdfjoin.pl
+++ pdfjoin.pl
@@ -22,31 +22,59 @@ if (-e $outputfile) {
 }
 
 my $url = shift;
+my $agent = WWW::Mechanize->new();
 
 print "Getting PDFs from $url...\n";
-
-my $agent = WWW::Mechanize->new();
-# Get loginpage
-$agent->get($url);
 
 rmtree('temp') if (-e 'temp');
 mkdir('temp');
 
-# Check all PDFs
+my @queue;
+push @queue, $url;
+
+# Get mainpage
+$agent->get($url);
 for my $link ($agent->links()) {
-	next unless $link->url() =~ /\.pdf$/;
-	# Skip MetaPress-crap
-	next if $link->url() =~ /MetaPress/;
-	my $fn = basename $link->url();
-	my $target = "temp/".$fn;
-	my $c = 0;
-	$target = "temp/".$fn.".".$c++ while (-e $target);
-	# New one, let's download
-	print "Downloading PDF $target...\n";
-	$agent->get($link->url_abs()->abs, ':content_file' => $target);
+	if ($link->url_abs()->abs =~ /^$url/ and $link->url_abs()->abs =~ /p_o/) {
+		print "Pushing ".$link->url_abs()->abs." to queue\n";
+		push @queue, $link->url_abs()->abs;
+	}
+}
+
+my %saw;
+my @unique_queue = grep(!$saw{$_}++, @queue);
+
+for my $c_url (@unique_queue) {
+	$agent->get($c_url);
+
+	print "Accessing $c_url...\n";
+
+	# Check all PDFs
+	for my $link ($agent->links()) {
+		next unless $link->url() =~ /\.pdf$/;
+		# Skip MetaPress-crap
+		next if $link->url() =~ /MetaPress/;
+		my $fn = basename $link->url();
+		my $prefix = "temp/";
+		my $target = $prefix.$fn;
+		my $c = 0;
+		if ($target =~ /front/) {
+			$prefix .= "00-";
+			$target = $prefix.$fn;
+		}
+		if ($target =~ /back/) {
+			$prefix .= "99-";
+			$target = $prefix.$fn;
+		}
+		next if ($target =~ /front|back/ and -e $target);
+		$target = "temp/".$fn.".".sprintf("%04d", $c++) while (-e $target);
+		# New one, let's download
+		print "Downloading PDF $target...\n";
+		$agent->get($link->url_abs()->abs, ':content_file' => $target);
+	}
 }
 
 print "Finished, joining...\n";
-system("pdftk temp/front* temp/full* temp/back* cat output $outputfile");
+system("pdftk temp/* cat output $outputfile");
 
 rmtree('temp');
