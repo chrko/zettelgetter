a0b8910db0a05527b7fb51491a7ea6235e38f1bd
Michael Stapelberg <michael@stapelberg.de>
2009-07-09 01:07:40 +0200

    Retab the file with new modeline, remove trailing whitespace

--- get.pl
+++ get.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+# vim:ts=4:sw=4:expandtab
 # Hint: You need libcrypt-ssleay-perl for this to work with SSL (necessary)
 
 use strict;
@@ -15,12 +16,12 @@ my $params = shift;
 my $update_mode = 0;
 
 if ($params) {
-	if ($params eq 'update' or $params eq '-u' or $params eq '--update') {
-		$update_mode = 1;
-	} elsif ($params eq 'help' or $params eq '-h' or $params eq '--help') {
-		print "Syntax: ./get.pl [update]\n";
-		exit;
-	}
+    if ($params eq 'update' or $params eq '-u' or $params eq '--update') {
+        $update_mode = 1;
+    } elsif ($params eq 'help' or $params eq '-h' or $params eq '--help') {
+        print "Syntax: ./get.pl [update]\n";
+        exit;
+    }
 }
 
 print "Logging into moodle...\n";
@@ -30,73 +31,73 @@ my $agent = WWW::Mechanize->new();
 $agent->get('https://elearning.uni-heidelberg.de/login/index.php');
 
 if (!$agent->form_number(2)) {
-	print "WARNING: Could not login - Either moodle on elearning.uni-heidelberg.de is broken or they changed something in the login process.\n";
+    print "WARNING: Could not login - Either moodle on elearning.uni-heidelberg.de is broken or they changed something in the login process.\n";
 } else {
-	# Submit loginform
-	$agent->submit_form(
-			form_number => 2,
-			fields => { username => $config::urz_user,
-				    password => $config::urz_pass, }
-		);
+    # Submit loginform
+    $agent->submit_form(
+            form_number => 2,
+            fields => { username => $config::urz_user,
+                    password => $config::urz_pass, }
+        );
 }
 
 sub get_digest {
-	my $file = shift;
-	open(FILE, $file) or die "Can't open $file";
-	binmode(FILE);
-	my $digest = Digest::MD5->new->addfile(*FILE)->hexdigest;
-	close(FILE);
+    my $file = shift;
+    open(FILE, $file) or die "Can't open $file";
+    binmode(FILE);
+    my $digest = Digest::MD5->new->addfile(*FILE)->hexdigest;
+    close(FILE);
 
-	return $digest;
+    return $digest;
 }
 
 sub get_url {
-	my ($name, $url, $target_path, $recurse) = @_;
-	my @additional_urls = ();
+    my ($name, $url, $target_path, $recurse) = @_;
+    my @additional_urls = ();
 
-	$agent->get($url);
+    $agent->get($url);
 
-	# Check all PDFs
-	for my $link ($agent->links()) {
-		my $abs_link = $link->url_abs()->abs;
-		push @additional_urls, $abs_link if $abs_link =~ /resource\/view\.php/;
-		next unless $link->url() =~ /\.(pdf|ps|txt|cpp|zip|tar|bz2)$/;
-		my $fn = basename $link->url();
-		my $target = $target_path."/".$fn;
-		if (-e $target) {
-			# Only update files if started in update mode
-			next unless $update_mode;
+    # Check all PDFs
+    for my $link ($agent->links()) {
+        my $abs_link = $link->url_abs()->abs;
+        push @additional_urls, $abs_link if $abs_link =~ /resource\/view\.php/;
+        next unless $link->url() =~ /\.(pdf|ps|txt|cpp|zip|tar|bz2)$/;
+        my $fn = basename $link->url();
+        my $target = $target_path."/".$fn;
+        if (-e $target) {
+            # Only update files if started in update mode
+            next unless $update_mode;
 
-			my $old_digest = get_digest($target);
-			# need to sandbox the call, else it kills the process on fetch error 
-			eval { $agent->get($abs_link, ':content_file' => $target); };
-			unless ($@) {
-				my $new_digest = get_digest($target);
-				print "\nDocument has changed! Check $target!\n\n" if ($old_digest ne $new_digest);
-			} else {
-				print "Couldn't load $fn\n";
-			}
-		} else {
-			# New one, let's download
-			print "Downloading new Document $fn...\n";
+            my $old_digest = get_digest($target);
+            # need to sandbox the call, else it kills the process on fetch error
+            eval { $agent->get($abs_link, ':content_file' => $target); };
+            unless ($@) {
+                my $new_digest = get_digest($target);
+                print "\nDocument has changed! Check $target!\n\n" if ($old_digest ne $new_digest);
+            } else {
+                print "Couldn't load $fn\n";
+            }
+        } else {
+            # New one, let's download
+            print "Downloading new Document $fn...\n";
 
-			# need to sandbox the call, else it kills the process on fetch error 
-			eval { $agent->get($abs_link, ':content_file' => $target); };
-			print "Couldn't load $fn\n" if $@;
-		}
-	}
+            # need to sandbox the call, else it kills the process on fetch error
+            eval { $agent->get($abs_link, ':content_file' => $target); };
+            print "Couldn't load $fn\n" if $@;
+        }
+    }
 
-	return unless $recurse;
+    return unless $recurse;
 
-	get_url($name, $_, $target_path, 0) for (@additional_urls);
+    get_url($name, $_, $target_path, 0) for (@additional_urls);
 }
 
 while (my ($name, $url) = each %config::urls) {
-	my $target_path = $config::target . $name;
+    my $target_path = $config::target . $name;
 
-	mkpath($target_path, { mode => 0755 }) unless (-e $config::target . $name);
+    mkpath($target_path, { mode => 0755 }) unless (-e $config::target . $name);
 
-	get_url($name, $url, $target_path, 1);
+    get_url($name, $url, $target_path, 1);
 }
 
 print "Finished.\n";
